f4759f40d300a37ac480448e1a62e83c760e8cdb
aapetsi <apetsiampiah@gmail.com>
2020-09-04 13:59:02 +0000

    connect graphql client

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Provider } from 'react-redux'
+import ApolloClient from 'apollo-boost'
+import {ApolloProvider} from 'react-apollo'
 import './App.css';
 import AppRouter from './routers/AppRouter';
 import store from './store/store'
 
+const client = new ApolloClient({
+  uri: 'http://localhost:4000/graphql'
+})
+
 const App = () => {
   return (
-    <Provider store={store()}>
-      <AppRouter />
-    </Provider>
+    <ApolloProvider client={client}>
+      <Provider store={store()}>
+        <AppRouter />
+      </Provider>
+    </ApolloProvider>
   );
 }
 
--- src/components/ContactList.jsx
+++ src/components/ContactList.jsx
@@ -1,10 +1,25 @@
 import React, { useEffect } from 'react'
 import {useDispatch, useSelector} from 'react-redux'
+import { gql } from 'apollo-boost'
+import { graphql} from 'react-apollo'
 import Contact from './Contact'
 
-const ContactList = () => {
+const getContactsQuery = gql`
+  {
+    contacts {
+      firstName
+    },
+    books {
+      name,
+      genre
+    }
+  }
+`
+
+const ContactList = (props) => {
+  console.log(props)
   const dispatch = useDispatch()
-  const contactState = useSelector((state: any) => state.contacts)
+  const contactState = useSelector((state) => state.contacts)
   useEffect(() => {
     console.log('mounted')
   })
@@ -19,4 +34,4 @@ const ContactList = () => {
   )
 }
 
-export default ContactList
+export default graphql(getContactsQuery)(ContactList)
