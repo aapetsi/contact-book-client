79ba6620c7e28a5d419537ad4d6f195e2a9be46b
aapetsi <apetsiampiah@gmail.com>
2020-09-04 02:51:48 +0000

    wip

--- src/components/ContactList.tsx
+++ src/components/ContactList.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import {useDispatch, useSelector} from 'react-redux'
 import Contact from './Contact'
 
 const ContactList = () => {
+  const dispatch = useDispatch()
+  const contactState = useSelector((state: any) => state.contacts)
+  useEffect(() => {
+    console.log('mounted')
+  })
   return (
     <div>
       <h2>Contact list component</h2>
+     
       <Contact id="1" />
       <Contact id="2" />
       <Contact id="3" />
